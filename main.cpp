6579c090b1264c142ffd9dfc042d56d7116c2a1b
Param Gattupalli <paramgattupalli@gmail.com>
2024-08-01 14:09:59 -0400

    Created Graph class with adjacency list implementation. Completed constructor, insertion, and bfs methods. Still need to implement Dijkstra's

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include "graph.h"
 
+using namespace std;
+int main(){
+    Graph graph;
+    graph.insertEdge("Messi", "Neymar");
+    graph.insertEdge("Neymar", "Vini");
+
+    graph.displayGraph();
+    vector<string> path = graph.bfs("Messi", "Vini");
+    for(string s: path){
+        cout << s << " ";
+    }
+    return 0;
+}
